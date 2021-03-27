9f9a534f22bed8871ee86d7a7733a13782a0bfdc
Relintai <relintai@protonmail.com>
2021-03-27 20:35:28 +0100

    Vector2, 3, and Rect2 now uses Math.

--- 02_math_osztaly/compile.sh
+++ 02_math_osztaly/compile.sh
@@ -10,6 +10,9 @@ fi
 #-Iinclude
 
 g++ -Wall -g -c math.cpp -o obj/math.o
+g++ -Wall -g -c vector2.cpp -o obj/vector2.o
+g++ -Wall -g -c vector3.cpp -o obj/vector3.o
+g++ -Wall -g -c rect2.cpp -o obj/rect2.o
 g++ -Wall -g -c main.cpp -o obj/main.o
 g++ -o bin/program obj/math.o obj/main.o
 
